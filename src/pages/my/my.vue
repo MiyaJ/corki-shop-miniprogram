035f25626e67ea984e283edca3d1ffb98ba837dd
CaiXiaowei <caixiaowei@renruihr.com>
2024-12-26 14:39:30 +0800

    1

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
+import { http } from '@/utils/http'
 
 const memberStore = useMemberStore()
+// 测试请求
+const getData = async () => {
+  const res = await http<number[]>({
+    url: '',
+    method: 'GET',
+    header: {},
+  })
+  console.log(res)
+}
 </script>
 
 <template>
@@ -20,6 +30,7 @@ const memberStore = useMemberStore()
       保存用户信息
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <button @tap="getData" size="mini" plain type="warn">测试请求</button>
   </view>
 </template>
 
