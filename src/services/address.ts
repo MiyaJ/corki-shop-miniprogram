9bff6acabef7733659802972212558a55256d5e0
CaiXiaowei <caixiaowei@renruihr.com>
2025-01-20 18:38:00 +0800

    feat: 地址管理

---
+++ src/services/address.ts
@@ -0,0 +1,46 @@
+import { http } from '@/utils/http'
+import type { AddressItem, AddressParams } from '@/types/address'
+
+/** 查询收货地址列表 */
+export const getMemberAddressAPI = () => {
+  return http<AddressItem[]>({
+    method: 'GET',
+    url: '/member/address',
+  })
+}
+
+/**
+ * 添加收货地址
+ */
+export const postMemberAddressAPI = (data: AddressParams) => {
+  return http({
+    method: 'POST',
+    url: '/member/address',
+    data,
+  })
+}
+
+/** 获取地址详情 */
+export const getMemberAddressByIdAPI = (id: string) => {
+  return http<AddressItem>({
+    method: 'GET',
+    url: `/member/address/${id}`,
+  })
+}
+
+/** 更新地址 */
+export const putMemberAddressByIdAPI = (id: string, data: AddressParams) => {
+  return http({
+    method: 'PUT',
+    url: `/member/address/${id}`,
+    data,
+  })
+}
+
+/** 删除地址 */
+export const deleteMemberAddressByIdAPI = (id: string) => {
+  return http({
+    method: 'DELETE',
+    url: `/member/address/${id}`,
+  })
+}
